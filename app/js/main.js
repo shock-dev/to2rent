c4ed2782ed8dc3308d87e4928ce9da9f00d41864
Andrey <shock.work@mail.ru>
2020-09-12 17:15:25 +0300

    #Added: Slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 
+    $('.slider').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="../img/arrow-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="../img/arrow-next.svg"></button>'
+    })
 
 
-  
 });
