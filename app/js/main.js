d71e71cef6932fde3b2024a6b817dbe32211b828
Andrey <shock.work@mail.ru>
2020-09-12 17:37:27 +0300

    #Added: Search form

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,8 @@ $(function(){
 
     $('.slider').slick({
         prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="../img/arrow-prev.svg"></button>',
-        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="../img/arrow-next.svg"></button>'
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="../img/arrow-next.svg"></button>',
+        fade: true
     })
 
 
