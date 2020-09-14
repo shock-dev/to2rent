99b18ecc479fd849cef44c9c335f50a0f3af98b1
Andrey <shock.work@mail.ru>
2020-09-14 17:25:01 +0300

    #Added: Adaptivity

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,12 @@ $(function(){
     $('.slider').slick({
         prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="../img/arrow-prev.svg"></button>',
         nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="../img/arrow-next.svg"></button>',
-        fade: true
+        autoplay: false,
+        autoplaySpeed: 3000,
     })
 
+    $('.burger').on('click', function () {
+        $('.menu__list').slideToggle()
+    })
 
 });
